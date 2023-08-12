05bd98bbe567b14ce2ed76ab156ab3be5cdd55ef
KennethTL <kennethtemba17@gmail.com>
2023-08-12 18:23:58 +0300

    Add task 8 deliverable

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is interpreted, interactive, object-oriented programming\
+        language that combines remarkable power with very clear syntax"
+str = str[36:64] + str[-22:-18] + " " + str[0:6]
+print(str)
